a3f36ca437050b5b47d86111179e715ae9646c92
Vojta Molda <vojta.molda@gmail.com>
2017-06-16 19:27:43 -0500

    Remove dependency of SCS types on ctypes - Reorganize telemetry tests

--- common/scstypes.py
+++ common/scstypes.py
@@ -1,78 +1,146 @@
-"""Definition of Python counterparts to C data structures used by the game telemetry plugin for ETS2/ATS"""
-
 import struct
-import ctypes
 
 
-class Structure(ctypes.Structure):
-    """Extension of `ctypes.Structure` that can unpack itself from byte arrays"""
+class struct_t(dict):
+    """ Structure that can unpack itself from byte arrays with fields accessible via dot.notation """
 
-    def __init__(self, buffer: bytes):
-        """Create a new instance and initialize all field values by unpacking the provided `bytes`"""
+    def __init__(self, values):
+        """ Initialize fields from a dictionary or from a list of values """
+        if isinstance(values, dict):
+            fields = values
+        else:
+            fields = {}
+            for (field_name, field_type), field_value in zip(self._fields_, values):
+                fields[field_name] = field_type(field_value)
+        super().__init__(fields)
+
+    def __getattr__(self, item):
+        """ Interface to access the structure members via dot.notation """
+        return self[item] if item in self else None
+
+    @classmethod
+    def unpack(cls, buffer: bytes) -> object:
+        """ Create a new instance and initialize all field values by unpacking the provided `bytes` """
+        if cls._fields_ is None or cls._type_ is None:
+            raise AttributeError("Attributes needed for unpacking are not implemented")
+
         if isinstance(buffer, bytes):
-            unpacked_values = struct.unpack(self._type_, buffer)
+            unpacked_values = struct.unpack(cls._type_, buffer)
             field_values = iter(unpacked_values)
         else:
             field_values = buffer
 
-        for (field_name, field_type) in self._fields_:
-            if issubclass(field_type, Structure):
-                substructure = field_type(buffer=field_values)
-                setattr(self, field_name, substructure)
+        members = {}
+        for (field_name, field_type) in cls._fields_:
+            if issubclass(field_type, struct_t):
+                substructure = field_type.unpack(field_values)
+                members[field_name] = substructure
             else:
                 primitive = field_type(next(field_values))
-                setattr(self, field_name, primitive)
-
-    def __repr__(self):
-        """Human readable representation for debugging, similar to `dict` """
-        strings = []
-        for (field_name, field_type) in self._fields_:
-            field_str = str(getattr(self, field_name, None))
-            strings.append("'{}': {}".format(field_name, field_str))
-        return "{" + ", ".join(strings) + "}"
+                members[field_name] = primitive
+        return cls(members)
 
 
-#  Python counterparts to primitive C types
-u32_t = ctypes.c_uint32  # `scs_u32_t`
-float_t = ctypes.c_float  # `scs_float_t`
-double_t = ctypes.c_double  # `scs_double_t`
-timestamp_t = ctypes.c_uint64  # `scs_timestamp_t`
+class u8_t(int):
+    """ Counterpart of `scs_u8_t` """
+    _type_ = 'B'
 
 
-class fvector_t(Structure):
-    """Python counterpart of `scs_fvector_value_t` structure"""
-    _fields_ = [('x', float_t),
-                ('y', float_t),
-                ('z', float_t)]
+class u16_t(int):
+    """ Counterpart of `scs_u16_t` """
+    _type_ = 'H'
+
+
+class s16_t(int):
+    """ Counterpart of `scs_s16_t` """
+    _type_ = 'h'
+
+
+class u32_t(int):
+    """ Counterpart of `scs_u32_t` """
+    _type_ = 'I'
+
+
+class s32_t(int):
+    """ Counterpart of `scs_s32_t` """
+    _type_ = 'i'
+
+
+class u64_t(int):
+    """ Counterpart of `scs_u64_t` """
+    _type_ = 'Q'
+
+
+class s64_t(int):
+    """ Counterpart of `scs_s64_t` """
+    _type_ = 'q'
+
+
+class float_t(float):
+    """ Counterpart of `scs_float_t` """
+    _type_ = 'f'
+
+
+class double_t(float):
+    """ Counterpart of `scs_double_t` """
+    _type_ = 'd'
+
+
+class timestamp_t(u64_t):
+    """ Counterpart of `scs_timestamp_t` """
+    pass
+
+
+class array_t(list):
+    """ Counterpart of `scs_array_t` array """
+    pass
+
+
+class float2_t(struct_t):
+    """ Counterpart of `scs_fixed2_t` structure """
+    _fields_ = [('x', float_t), ('y', float_t)]
+
+
+class float3_t(struct_t):
+    """ Counterpart of `scs_fixed3_t` structure """
+    _fields_ = [('x', float_t), ('y', float_t), ('z', float_t)]
+
+
+class float4_t(tuple):
+    """Counterpart of `scs_float4_t` structure"""
+    pass
+
+
+class quaternion_t(struct_t):
+    """ Counterpart of `scs_quaternion_t` structure """
+    _fields_ = [('w', float_t), ('x', float_t), ('y', float_t), ('z', float_t)]
+
+
+class fvector_t(struct_t):
+    """ Counterpart of `scs_fvector_value_t` structure """
+    _fields_ = [('x', float_t), ('y', float_t), ('z', float_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
 
 
-class dvector_t(Structure):
-    """Python counterpart of `scs_dvector_value_t` structure"""
-    _fields_ = [('x', double_t),
-                ('y', double_t),
-                ('z', double_t)]
+class dvector_t(struct_t):
+    """ Counterpart of `scs_dvector_value_t` structure """
+    _fields_ = [('x', double_t), ('y', double_t), ('z', double_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
 
 
-class euler_t(Structure):
-    """Python counterpart of `scs_value_euler_t` structure"""
-    _fields_ = [('heading', float_t),
-                ('pitch', float_t),
-                ('roll', float_t)]
+class euler_t(struct_t):
+    """ Counterpart of `scs_value_euler_t` structure """
+    _fields_ = [('heading', float_t), ('pitch', float_t), ('roll', float_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
 
 
-class fplacement_t(Structure):
-    """Python counterpart of `scs_value_fplacement_t` structure"""
-    _fields_ = [('position', fvector_t),
-                ('orientation', euler_t)]
+class fplacement_t(struct_t):
+    """ Counterpart of `scs_value_fplacement_t` structure """
+    _fields_ = [('position', fvector_t), ('orientation', euler_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
 
 
-class dplacement_t(Structure):
-    """Python counterpart of `scs_value_dplacement_t` structure"""
-    _fields_ = [('position', dvector_t),
-                ('orientation', euler_t),
-                ('_padding', u32_t)]
+class dplacement_t(struct_t):
+    """ Counterpart of `scs_value_dplacement_t` structure """
+    _fields_ = [('position', dvector_t), ('orientation', euler_t), ('_padding', u32_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
--- telemetry/packet.py
+++ telemetry/packet.py
@@ -1,10 +1,12 @@
-"""Definition of Python counterparts to C data structures published by the ETS2/ATS game telemetry plugin"""
+import zmq
+import struct
+import unittest
 
 import common.scstypes as scstypes
 
 
-class Packet(scstypes.Structure):
-    """Python counterpart of the data structure published by the telemetry SDK pluging"""
+class Packet(scstypes.struct_t):
+    """ Counterpart of the data structure sent by the ETS2/ATS telemetry plugin """
     _fields_ = [('placement', scstypes.dplacement_t),
                 ('linear_velocity', scstypes.dvector_t),
                 ('angular_velocity', scstypes.dvector_t),
@@ -13,3 +15,79 @@ class Packet(scstypes.Structure):
                 ('simulation_time', scstypes.timestamp_t),
                 ('paused_simulation_time', scstypes.timestamp_t)]
     _type_ = "".join([field_type._type_ for (field_name, field_type) in _fields_])
+
+
+# region Unit Tests
+
+
+class TestTelemetry(unittest.TestCase):
+
+    def test_unpack(self):
+        struct_format = 'ddd fff I ddd ddd d QQQ'.replace(' ', '')
+        self.assertEqual(Packet._type_, struct_format)
+
+        struct_values = list(range(len(struct_format)))
+        struct_packed = struct.pack(struct_format, *struct_values)
+        packet = Packet.unpack(struct_packed)
+
+        self.assertEqual(packet.placement.position.x, 0)
+        self.assertEqual(packet.placement.position.y, 1)
+        self.assertEqual(packet.placement.position.z, 2)
+        self.assertEqual(packet.placement.orientation.heading, 3)
+        self.assertEqual(packet.placement.orientation.pitch, 4)
+        self.assertEqual(packet.placement.orientation.roll, 5)
+        self.assertEqual(packet.placement._padding, 6)
+
+        self.assertEqual(packet.linear_velocity.x, 7)
+        self.assertEqual(packet.linear_velocity.y, 8)
+        self.assertEqual(packet.linear_velocity.z, 9)
+
+        self.assertEqual(packet.angular_velocity.x, 10)
+        self.assertEqual(packet.angular_velocity.y, 11)
+        self.assertEqual(packet.angular_velocity.z, 12)
+
+        self.assertEqual(packet.speed, 13)
+        self.assertEqual(packet.render_time, 14)
+        self.assertEqual(packet.simulation_time, 15)
+        self.assertEqual(packet.paused_simulation_time, 16)
+
+    @unittest.skip("Game has to be started and shutdown manually")
+    def test_telemetry(self):
+        # copy libtelemetry.so into $(HOME)/Library/Application Support/Steam/steamapps/common/American Truck Simulator/American Truck Simulator.app/Contents/MacOS/plugins/
+        # start this test
+        # run $(HOME)/Library/Application Support/Steam/steamapps/common/American Truck Simulator/American Truck Simulator.app/Contents/MacOS/amtrucks
+        # get into a truck and drive for a moment
+        # exit the game
+
+        event_socket = zmq.Context().socket(zmq.SUB)
+        event_socket.setsockopt(zmq.SUBSCRIBE, bytes())
+        event_socket.connect('ipc:///tmp/event.ipc')
+        telemetry_socket = zmq.Context().socket(zmq.SUB)
+        telemetry_socket.setsockopt(zmq.SUBSCRIBE, bytes())
+        telemetry_socket.connect('ipc:///tmp/telemetry.ipc')
+
+        config, start, telemetry, pause, shutdown = False, False, False, False, False
+        while True:
+            recieved_event = event_socket.recv()
+            if recieved_event == b'config':
+                config = True
+            if recieved_event == b'start':
+                start = True
+                for counter in range(100):
+                    message = telemetry_socket.recv()
+                    packet = Packet(message)
+                telemetry = True
+            if recieved_event == b'pause':
+                pause = True
+            if recieved_event == b'shutdown':
+                shutdown = True
+                break
+
+        self.assertTrue(config)
+        self.assertTrue(start)
+        self.assertTrue(telemetry)
+        self.assertTrue(pause)
+        self.assertTrue(shutdown)
+
+
+# endregion
