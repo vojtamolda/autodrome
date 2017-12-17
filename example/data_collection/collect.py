ea1432820ae0b9042de9284ec3bcffafd6283e40
Vojta Molda <vojta.molda@gmail.com>
2017-12-16 23:52:02 -0600

    Amend Examples

---
+++ example/data_collection/collect.py
@@ -0,0 +1,20 @@
+import argparse
+from autodrome.simulator import ETS2, ATS
+
+
+Simulators = {'ETS2': ETS2, 'ATS': ATS}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run ETS2/ATS for data collection")
+    parser.add_argument('simulator', choices=Simulators,
+                        help="Game to run (i.e. ETS2 or ATS)")
+    parser.add_argument('-m', '--map', default=None,
+                        help="Map to drive on (i.e. 'europe' for ETS2 or 'usa' for ATS)")
+    args = parser.parse_args()
+
+    with Simulators[args.simulator]() as simulator:
+        if args.map is not None:
+            simulator.command('preview {}'.format(args.map))
+    while simulator.process.poll() is None:
+        reply = simulator.telemetry.recv()
+        # TODO: Store data here
---
+++ example/readme.md
@@ -0,0 +1,26 @@
+
+# Autodrome Examples
+
+
+
+## Data Collection - [`data_collection/`](data_collection/)
+
+TODO: Text
+
+![Data Collection](data_collection/data_collection.jpg)
+
+
+
+## OpenAI Gym - [`gym/`](gym/)
+
+TODO: Text
+
+![Open AI Gym](gym/gym.jpg)
+
+
+
+## Visualization - [`visualization/`](visualization/)
+
+TODO: Text
+
+![Visualization](visualization/visualization.jpg)
